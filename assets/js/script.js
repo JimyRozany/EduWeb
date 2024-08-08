ba330e31573f6e14240efbc631ad299a67a18565
JimyRozany <40678835+JimyRozany@users.noreply.github.com>
2024-08-09 02:25:35 +0300

    Add a new section and edit the authentication view

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,4 @@
-'use strict';
-
-
+"use strict";
 
 /**
  * add event on element
@@ -14,9 +12,7 @@ const addEventOnElem = function (elem, type, callback) {
   } else {
     elem.addEventListener(type, callback);
   }
-}
-
-
+};
 
 /**
  * navbar toggle
@@ -30,18 +26,16 @@ const overlay = document.querySelector("[data-overlay]");
 const toggleNavbar = function () {
   navbar.classList.toggle("active");
   overlay.classList.toggle("active");
-}
+};
 
 addEventOnElem(navTogglers, "click", toggleNavbar);
 
 const closeNavbar = function () {
   navbar.classList.remove("active");
   overlay.classList.remove("active");
-}
+};
 
 addEventOnElem(navLinks, "click", closeNavbar);
-
-
 
 /**
  * header active when scroll down to 100px
@@ -58,82 +52,86 @@ const activeElem = function () {
     header.classList.remove("active");
     backTopBtn.classList.remove("active");
   }
-}
+};
 
 addEventOnElem(window, "scroll", activeElem);
 
-
-
-
 // ************ show & hide login card ************
-const loginBtn = document.getElementById("login-btn")
-const loginArea = document.getElementById("login-area")
+const loginBtn = document.getElementById("login-btn");
+const loginArea = document.getElementById("login-area");
 
-
-
-
-const wrapper = document.querySelector('.wrapper-login');
-const loginLink = document.querySelector('.login-link');
-const registerLink = document.querySelector('.register-link');
-const btnPopup = document.querySelector('.btnlogin-popup');
-const iconClose = document.querySelector('.icon-close');
-registerLink.addEventListener('click',() => {
-    wrapper.classList.add('active');
+const wrapper = document.querySelector(".wrapper-login");
+const loginLink = document.querySelector(".login-link");
+const registerLink = document.querySelector(".register-link");
+const btnPopup = document.querySelector(".btnlogin-popup");
+const iconClose = document.querySelector(".icon-close");
+registerLink.addEventListener("click", () => {
+  wrapper.classList.add("active");
 });
 
-loginLink.addEventListener('click',() => {
-    wrapper.classList.remove('active');
+loginLink.addEventListener("click", () => {
+  wrapper.classList.remove("active");
 });
 
-loginBtn.addEventListener('click',() => {
-    // wrapper.classList.add('active-popup');
-    loginArea.style.transform = "scale(1)";
-    wrapper.style.transform = "scale(1)";
-
+loginBtn.addEventListener("click", () => {
+  // wrapper.classList.add('active-popup');
+  loginArea.style.transform = "scale(1)";
+  wrapper.style.transform = "scale(1)";
 });
-iconClose.addEventListener('click',() => {
-    // wrapper.classList.remove('active-popup');
-    wrapper.style.transform = "scale(0)";
+iconClose.addEventListener("click", () => {
+  // wrapper.classList.remove('active-popup');
+  wrapper.style.transform = "scale(0)";
 
-    setTimeout(() => {
-      loginArea.style.transform = "scale(0)";
-    }, 1000);
+  setTimeout(() => {
+    loginArea.style.transform = "scale(0)";
+  }, 1000);
 });
 
 // ************ end show & hide login card ************
-
 
 // ************ show & hide payment card ************
 
 const paymentArea = document.getElementById("payment-area");
 const closePaymentBtn = document.getElementById("payment-close-btn");
 
-
-let showPaymentCard = (e)=>{
+let showPaymentCard = (e) => {
   // console.log(e.children[2].children[1].children[0].innerHTML);
-  let price = e.children[2].children[3].value ;
-  let title = e.children[2].children[1].children[0].innerHTML ;
-  
+  let price = e.children[2].children[3].value;
+  let title = e.children[2].children[1].children[0].innerHTML;
 
   const cardTitle = document.querySelector(".payment-card-title");
   const cardPrice = document.querySelector(".payment-card-price");
 
-  cardTitle.innerHTML = title ;
-  cardPrice.innerHTML = "$" + price ;
+  cardTitle.innerHTML = title;
+  cardPrice.innerHTML = "$" + price;
 
-  paymentArea.style.transform =  "scale(1)";
+  paymentArea.style.transform = "scale(1)";
+};
 
-}
-
-
-closePaymentBtn.addEventListener("click" ,()=>{
-  paymentArea.style.transform =  "scale(0)";
-})
-
-
+closePaymentBtn.addEventListener("click", () => {
+  paymentArea.style.transform = "scale(0)";
+});
 
 // ************ end show & hide payment card ************
 
+// ************ language section ************
 
+const showMoreCoursesSection = () => {
+  const langSection = document.getElementById("lang-course");
+  const moreCoursesArrow = document.getElementById("more-courses-arrow");
+  const moreCoursesBtn = document.getElementById("more-courses-btn");
+console.log(moreCoursesBtn.children);
+  if (langSection.classList.contains("show-lang-course")) {
+    langSection.classList.remove("show-lang-course");
+    moreCoursesArrow.style.rotate = "0deg";
+    moreCoursesBtn.children[0].innerHTML = "Browse more courses";
+  } else {
+    langSection.classList.add("show-lang-course");
+    
+    moreCoursesArrow.style.rotate = "-90deg";
+    moreCoursesBtn.children[0].innerHTML = "Browse less courses" ;
 
+  }
+};
 
+// ************ end language section ************
